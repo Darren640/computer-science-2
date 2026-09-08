2feec142d871a3bdfc6881b7ceb95de7f8265e16
Darren640 <darrenlopez956@gmail.com>
2026-09-08 19:49:54 +0000

    doggame

---
+++ reverseHiLowgame.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>   // Used to generate random number using rand method
+#include <string> // Used to be able to use string
+#include <time.h>  // Used to reset the machine time to generate different random numbers
+#include <random>  // Gives access to even better random number generators, if you want
+
+using namespace std;
+
+
+
+int main()
+{
+    srand(static_cast<int>(time(NULL)));
+    cout << "Enter a postive number: " << endl;
+
+    int n;
+    cin >> n;
+
+    cout << "Write down a number between 1 and n with penicl on sheet of paper " << endl;
+
+    while(true)
+    {
+        if(n > )
+    }
+}
