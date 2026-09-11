724a1a980df8fdd5449e84be0060df866b9ce550
Darren640 <darrenlopez956@gmail.com>
2026-09-11 00:07:47 +0000

    add basic calculator class with undo and redo functionality

---
+++ calulator.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+using namespace std;
+
+
+
+class Calculator
+    {
+        private:
+            double value;
+            vector<double> undoStack;
+            vector<double> redoStack;
+        public: 
+
+        Calculator();
+        Calculator(double initialValue);
+        {
+
+        }
+        void clear();
+        {
+            value = 0;
+        }
+        void add(double value);
+        {
+            
+        }
+        void subtract(double value);
+        {
+
+        }
+        void multiply(double value);
+        {
+
+        }
+        void divide(double value);
+        {
+
+        }
+        double display() const;
+        {
+
+        }
+        void undo();
+        {
+
+        }
+        void redo();
+        {
+
+        }
+        Calculator operator+(const Calculator& other) const;
+        {
+
+        }
+        
+    }
+
+
+
+
+    int main()
+    {
+        Calculator mycalc;
+	
+	mycalc.clear();
+	mycalc.add(4.52);
+	mycalc.add(3.789);
+	mycalc.divide(2.6);
+	mycalc.multiply(3.12);
+	mycalc.subtract(2.678);
+	cout << mycalc.display() << endl;       // prints out "7.2928"
+	mycalc.clear();
+	mycalc.add(5.0);
+	cout << mycalc.display() << endl;       // prints out "5"
+
+	// add a constructor
+	Calculator calc1;
+	cout << calc1.display() << endl;  //prints out 0
+
+	// add a parameterized constructor
+	Calculator calc2(5);
+	cout << calc2.display() << endl; //prints out 5
+
+	//Optional Challenege #1: Define calculator addition (overload the '+' operator)
+	Calculator calc3(7);
+	calc1 = calc2 + calc3;
+	cout << calc1.display() << endl;  //prints out 12
+
+	//Optional Challenge #2: Create an 'undo' method for the calculator
+	mycalc.undo();
+	mycalc.undo();
+	cout << mycalc.display()<< endl;  //prints out 7.2928
+
+	//Optional Challenge #3: Create a 'redo' method for the calculator
+	mycalc.redo();
+	mycalc.redo();
+	cout << mycalc.display()<< endl;  //prints out 5
+
+    }
