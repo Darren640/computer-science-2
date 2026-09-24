9bbdf4a3484e53e0c6497e0f3268cca7907d27b5
Darren640 <darrenlopez956@gmail.com>
2026-09-24 20:04:04 +0000

    setting up the hw

---
+++ homework assignments/connect4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void DrawBoard()
+{
+
+}
+bool GameOver()
+{
+
+}
+char Winner()
+{
+
+}
+bool MakeMover()
+{
+
+}
+int Connect4()
+{
+
+}
+int main()
+[
+    int row[6];
+    int column[7];
+]
